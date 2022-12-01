eb3a91acdf5d865a75d61f80958718a82b7ef991
Martijn van der Blom <m.van.der.blom@blueriq.com>
2022-12-01 19:18:32 +0100

    Fix for day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val dayName = "Day01"
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part1(input: List<Int?>): Int = input
+            .mapIndexedNotNull { index, calories ->
+                if (index == 0 || index == input.size - 1 || calories == null) index else null
+            }
+            .windowed(2, 1)
+            .print()
+            .maxOf { (from, to) -> input.subList(from, to).filterNotNull().sum() }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    fun part2(input: List<Int?>): Int = input.size
+
+    val testInput = readInput("${dayName}_test")
+            .map { it.toIntOrNull() }
+
+    val testOutputPart1 = part1(testInput)
+    testOutputPart1 isEqualTo 24000
+
+    val input = readInput(dayName)
+            .map { it.toIntOrNull() }
+
+    val outputPart1 = part1(input)
+    outputPart1 isEqualTo 69795
+
+    val testOutputPart2 = part2(testInput)
+    testOutputPart2 isEqualTo null
+
+    val outputPart2 = part2(input)
+    outputPart2 isEqualTo null
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,20 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+infix fun Any.isEqualTo(expected: Any?): Boolean {
+    if (expected == null) {
+        error("Expectation not set...")
+    }
+
+    if (!this.equals(expected)) {
+        error("Expected $expected, but got $this")
+    }
+    println(this)
+    return true
+}
+
+fun <T> T.print(): T {
+    println(this)
+    return this;
+}
