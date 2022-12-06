3ef686becb32e65f7f50faf3db6750c21d44ca23
Martijn van der Blom <m.van.der.blom@blueriq.com>
2022-12-06 09:22:24 +0100

    Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val dayName = 6.toDayName()
+
+    fun part1(input: String, windowSize: Int = 4): Int = input
+            .windowed(windowSize)
+            .indexOfFirst { it.toSet().size == windowSize } + windowSize
+
+    val testInput = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    val input = readInput(dayName).get(0)
+
+    // Part 1
+
+    val testOutputPart1 = part1(testInput)
+    testOutputPart1 isEqualTo 7
+
+    val outputPart1 = part1(input)
+    outputPart1 isEqualTo 1238
+
+    // Part 2
+
+    val testOutputPart2 = part1(testInput, 14)
+    testOutputPart2 isEqualTo 19
+
+    val outputPart2 = part1(input, 14)
+    outputPart2 isEqualTo null
+}
