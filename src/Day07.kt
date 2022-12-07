dfd6d5faa9f01f6f1c41fe24ee45dec8e35a03f5
Martijn van der Blom <m.van.der.blom@blueriq.com>
2022-12-07 19:56:21 +0100

    cleanup Day 7

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,65 +1,78 @@
-data class File(val name: String, val size: Long)
-data class Dir(val name: String, val subDirs: MutableList<Dir>, val files: MutableList<File>, val parent: Dir? = null) {
-    fun size(): Long {
+import java.util.*
+
+data class File(val name: String, val size: Int)
+data class Dir(val name: String, val subDirs: MutableList<Dir>, val files: MutableList<File>) {
+    fun size(): Int {
         return files.sumOf { it.size } + subDirs.sumOf { it.size() }
     }
     companion object {
-        fun withName(name: String, parent: Dir? = null): Dir {
-            return Dir(name, mutableListOf(), mutableListOf(), parent)
+        fun withName(name: String): Dir {
+            return Dir(name, mutableListOf(), mutableListOf())
         }
     }
-
-    override fun toString(): String {
-        return name + " : " + size()
-    }
 }
-
+data class FileSystem (val rootDir: Dir, val allDirs: List<Dir>)
 
 fun main() {
     val dayName = 7.toDayName()
 
-    fun part1(input: List<String>): Long {
-        var cd: Dir? = null
+    fun parseInput(input: List<String>): FileSystem {
+        var cd = Stack<Dir>()
+        var root: Dir? = null
         val allDirs = mutableListOf<Dir>();
 
         input
-            .filter { it != "$ ls" }
-            .forEach { it ->
-                println(it)
-                when {
-                    it == "$ cd .." -> {
-                        cd = cd!!.parent
-                    }
-                    it.startsWith("$ cd ") -> {
-                        val dirName = it.substring(5)
+                .filter { it != "$ ls" }
+                .forEach { it ->
+                    when {
+                        it == "$ cd .." -> {
+                            cd.pop()
+                        }
+                        it.startsWith("$ cd ") -> {
+                            val dirName = it.substring(5)
 
-                        if ( cd == null) {
-                            val dir = Dir.withName(dirName)
-                            cd = dir
-                            allDirs.add(dir);
-                        } else {
-                            cd = cd!!.subDirs.find { it.name == dirName}
+                            val dir = if (cd.size == 0) {
+                                val dir = Dir.withName(dirName)
+                                root = dir
+                                allDirs.add(dir);
+                                dir
+                            } else cd.peek().subDirs.single { it.name == dirName }
+
+                            cd.push(dir)
+                        }
+                        it.startsWith("dir ") -> {
+                            val dir = Dir.withName(it.substring(4))
+                            allDirs.add(dir)
+                            cd.peek().subDirs.add(dir)
+                        }
+                        else -> {
+                            val (size, name) = it.split(" ")
+                            cd.peek().files.add(File(name, size.toInt()))
                         }
                     }
-                    it.startsWith("dir ") -> {
-                        val dir = Dir.withName(it.substring(4), cd!!)
-                        allDirs.add(dir)
-                        cd!!.subDirs.add(dir)
-                    }
-                    else -> {
-                        cd!!.files.add(File(it.split(" ")[1], it.split(" ")[0].toLong()))
-                    }
                 }
-            }
-        return allDirs
-                .sortedByDescending { it.size() }
-                .showMe()
-                .filter { it.size() < 100000 }
-                .showMe()
-                .sumOf { it.size() }
+        return FileSystem(root!!, allDirs)
     }
 
-    fun part2(input: List<String>): Int = input.size
+    fun part1(input: List<String>): Int {
+        return parseInput(input).allDirs
+                .map { it.size() }
+                .filter { it < 100000 }
+                .sortedDescending()
+                .sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        val fileSystem = parseInput(input)
+        val usedSpace = fileSystem.rootDir.size()
+        val availableSpace = 70000000 - usedSpace
+        val neededSpace = 30000000 - availableSpace
+        return fileSystem.allDirs
+                .map { it.size() }
+                .sorted()
+                .showMe()
+                .first { it > neededSpace }
+    }
 
     val testInput = readInput("${dayName}_test")
     val input = readInput(dayName)
@@ -67,16 +80,16 @@ fun main() {
     // Part 1
 
     val testOutputPart1 = part1(testInput)
-    testOutputPart1 isEqualTo 95437L
+    testOutputPart1 isEqualTo 95437
 
     val outputPart1 = part1(input)
-    outputPart1 isEqualTo 1453349L
+    outputPart1 isEqualTo 1453349
 
     // Part 2
 
     val testOutputPart2 = part2(testInput)
-    testOutputPart2 isEqualTo null
+    testOutputPart2 isEqualTo 24933642
 
     val outputPart2 = part2(input)
-    outputPart2 isEqualTo null
+    outputPart2 isEqualTo 2948823
 }
